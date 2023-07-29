614fe992336cf313849562e0167e6bb76b74b471
Sergey Lebedev <ob430y@yandex.ru>
2023-07-29 20:44:53 +0700

    Update models.py

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,10 @@
 from enum import auto
 
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
+
 
 # Create your models here.
 
@@ -34,8 +38,19 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     def __str__(self):
-        return f'ig = {self.id} title = {self.title} description = {self.description} price = {self.price}'
+        return f'id = {self.id} title = {self.title} description = {self.description} price = {self.price}'
 
     class Meta:
         db_table = "advertisement"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
