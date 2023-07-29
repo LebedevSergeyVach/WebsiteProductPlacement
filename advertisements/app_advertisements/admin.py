772e3f3ed8d0bf7fc2e3975bcfba4b1c4ce97835
Sergey Lebedev <ob430y@yandex.ru>
2023-07-29 20:45:19 +0700

    Update admin.py

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Advertisement
+
 
 # Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auctions', 'created_date', 'updated_at']
+    list_filter = ['auctions', 'created_at', 'updated_at']
+    actions =['make_auctions_as_false', 'make_auctions_as_true']
+    fieldsets = (
+        ('Общая информация', {
+            'fields': ('title', 'description')
+        }),
+
+        ('Цены', {
+            'fields': ('price', 'auctions'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auctions_as_false(self, request, queryset):
+        queryset.update(auctions=False)
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auctions_as_true(self, request, queryset):
+        queryset.update(auctions=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
