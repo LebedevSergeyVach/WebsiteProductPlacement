f8fa87dbc751b8171153d926529996f928b4d113
Sergey Lebedev <ob430y@yandex.ru>
2023-07-31 05:11:14 +0700

    Update views.py urls.py

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement, advertisement_post, login, profile, register
 
 
 urlpatterns = [
+    path("search/", index, name="search"),
+
     path('', index, name="main-page"),
     path("top-sellers/", top_sellers, name="top-sellers"),
-    path("search/", index, name="search"),
+
+    path("advertisement/", advertisement, name="advertisement"),
+    path("advertisement-post/", advertisement_post, name="advertisement-post"),
+
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("register/", register, name="register"),
 ]
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -8,3 +8,23 @@ def index(request):
 
 def top_sellers(request):
     return render(request, "top-sellers.html")
+
+
+def advertisement(request):
+    return render(request, "advertisement.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
+
+
+def advertisement_post(request):
+    return render(request, "advertisement-post.html")
+
+
+def register(request):
+    return render(request, "register.html")
