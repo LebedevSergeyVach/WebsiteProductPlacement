877dda578bc3c938912c6d8a1f30561bb4726286
GigantPro <pochtagigantpro@gmail.com>
2023-08-02 15:17:48 +0300

    Add config

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from .config import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@-72fdu1zvgu(ak4ll7zh!=(_-y3b#4(&3x)uq-3qac0rpb^me'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.DEBUG
 
 ALLOWED_HOSTS = [
     'garage.xiver.ru'
