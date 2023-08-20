0d380f8a6272b62c087ccb75928e0e3a583278a0
LebedevSergeyV <ob430y@yandex.ru>
2023-08-21 00:30:59 +0700

    Fix secrets_settings.py

--- advertisements/advertisements/secrets_settings.py
+++ advertisements/advertisements/secrets_settings.py
@@ -1,4 +1,4 @@
 """Placing the django secret key for the server"""
 
-SECRET_KEY='django-insecure-@-72fdu1zvg&)(#@#%!^^$^$^%&!)($#%u(abhldfghbjkl6987^&()@$(_(U())HU^me'
+SECRET_KEY='django-insecure-@-7sdfghsdfhds'
 run_server = '* Website is now running normally'
