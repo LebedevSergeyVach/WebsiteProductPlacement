7603f162b793833223d1294bd1cdcb42f1660466
LebedevSergeyV <ob430y@yandex.ru>
2023-09-07 15:10:09 +0700

    Add data/ & Update docker-compose.yml

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'advertisements.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../data/db.sqlite3',
         # "USER": "",
         # "PASSWORD": "",
         # "HOST": "127.0.0.1:8000",
