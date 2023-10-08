be5c7bf073f2a1212af9a851d20e1bf11cec9fbe
LebedevSergeyV <ob430y@yandex.ru>
2023-10-08 17:23:23 +0700

    Update base.html links

--- advertisements/manage.py
+++ advertisements/manage.py
@@ -14,7 +14,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-            'PIZDA YOUR WEBSITE ON SERVER !!!'
+            'PIZDA NAXUI YOUR WEBSITE ON SERVER !!!'
         ) from exc
     execute_from_command_line(sys.argv)
 
