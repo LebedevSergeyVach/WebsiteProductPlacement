d01580b365cb18152bd169c98c36561b5319c9d9
LebedevSergeyV <ob430y@yandex.ru>
2023-10-16 18:37:06 +0700

    Fix

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config.SECRET_KEY
-SECRET_KEY = os.environ.get(SECRET_KEY, SECRET_KEY)
+SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config.DEBUG
