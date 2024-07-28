773b6267ba8a4f6fefde21313e1778c4d7f6327a
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-07-28 23:02:15 +0700

    Update forms.py

--- advertisements/app_advertisements/forms.py
+++ advertisements/app_advertisements/forms.py
@@ -11,22 +11,42 @@ class AdvertisementForm(forms.ModelForm):
         ]
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}),
-            'description': forms.Textarea(attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}),
-            'image_1': forms.FileInput(attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}),
-            'image_2': forms.FileInput(attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}),
-            'image_3': forms.FileInput(attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}),
-            'price': forms.NumberInput(attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}),
-            'auction': forms.CheckboxInput(attrs={'class': 'form-check-input btn-outline-primary'}),
-            'contact': forms.TextInput(attrs={'class': 'form-control bg-dark text-light btn-outline-primary'})
+            'title': forms.TextInput(
+                attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}
+            ),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}
+            ),
+            'image_1': forms.FileInput(
+                attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}
+            ),
+            'image_2': forms.FileInput(
+                attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}
+            ),
+            'image_3': forms.FileInput(
+                attrs={'class': 'form-control bg-dark text-primary btn-outline-primary'}
+            ),
+            'price': forms.NumberInput(
+                attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}
+            ),
+            'auction': forms.CheckboxInput(
+                attrs={'class': 'form-check-input btn-outline-primary'}
+            ),
+            'contact': forms.TextInput(
+                attrs={'class': 'form-control bg-dark text-light btn-outline-primary'}
+            )
         }
 
     def clean_title(self):
         """Validate that the title is not empty and does not start with a question mark"""
         title = self.cleaned_data.get('title')
+        description = self.cleaned_data.get('description')
 
         if title.startswith('?'):
             raise forms.ValidationError("Title cannot start with a question mark")
 
-        return title
+        if description.startswith('?'):
+            raise forms.ValidationError("Title cannot start with a question mark")
+
+        return title, description
     
